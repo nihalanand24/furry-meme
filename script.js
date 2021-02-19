eb6a0fd24601977619597c5043e4334558261225
nihalanand <nihalanand@gmail.com>
2021-02-19 00:51:05 -0500

    cleaned up code

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-/*
-
-Friendly Weather App
-
-*/
-
-// console.log(fetch('https://ipinfo.io/'));
 
 const app = {};
 
@@ -30,33 +23,3 @@ const coordinates = fetch('https://freegeoip.app/json/')
                         app.pEl.textContent = `Your GPS Coordinates are ${app.lat}, ${app.lon}.`
                     });
             });
-
-
-
-
-
-
-// const url = new URL('http://proxy.hackeryou.com');
-// url.search = new URLSearchParams({
-//     reqUrl: 'https://freegeoip.app/json/'
-// });
-
-// fetch(url).then(res => res.json())
-//     .then(jsonRes => console.log(jsonRes));
-
-
-// axios({
-//     method:'GET',
-//     url: 'http://proxy.hackeryou.com',
-//     //OR url: 'https://proxy.hackeryou.com',
-//     responseType:'json',
-//     params: {
-//       reqUrl: 'https://freegeoip.app/json/',
-//       proxyHeaders: {
-//         'header_params': 'value'
-//       },
-//     },
-//       xmlToJSON: false
-//     }).then((res) => {
-//       console.log(res);
-//     });
